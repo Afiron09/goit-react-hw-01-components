42ee970a672433d2a294fc6b645d0d0f0efe3bc1
Afiron09 <comixgod@gmail.com>
2023-12-18 00:16:44 +0200

    1

---
+++ src/components/App/App.jsx
@@ -0,0 +1,19 @@
+import user from "path/to/user.json";
+import { Profile } from "../Profile/Profile";
+import { Statistics } from "../Statistics/Statistics";
+import data from "path/to/data.json";
+
+export const App = () => {
+  return (
+    <div>
+      <Profile
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+      />
+      <Statistics title="Upload stats" stats={data} />;
+    </div>
+  );
+};
