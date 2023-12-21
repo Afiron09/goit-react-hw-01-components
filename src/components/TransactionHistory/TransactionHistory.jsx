1a443de3b65d6236e560dc5898fed785b74014ee
Afiron09 <comixgod@gmail.com>
2023-12-21 15:45:43 +0200

    5

--- src/components/TransactionHistory/TransactionHistory.jsx
+++ src/components/TransactionHistory/TransactionHistory.jsx
@@ -1,4 +1,5 @@
 import css from "./TransactionHistory.module.css";
+import PropTypes from "prop-types";
 
 export const TransactionHistory = ({ items }) => {
   return (
@@ -21,4 +22,15 @@ export const TransactionHistory = ({ items }) => {
       ))}
     </table>
   );
+};
+
+TransactionHistory.propTypes = {
+  items: PropTypes.arrayOf(
+    PropTypes.exact({
+      id: PropTypes.string.isRequired,
+      type: PropTypes.string.isRequired,
+      amount: PropTypes.string.isRequired,
+      currency: PropTypes.string.isRequired,
+    })
+  ),
 };
